1ed0eb02332d1a1843ce0e6ade4c6b0ca4ed42b9
sebastianknopf <mail@sebastian-knopf.de>
2024-10-17 18:33:00 +0200

    Übung / Vorlesung I

---
+++ app/src/main/java/de/hka/ws2425/MainActivity.java
@@ -0,0 +1,28 @@
+package de.hka.ws2425;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hka.ws2425.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+
+        }
+    }
+}
---
+++ app/src/main/java/de/hka/ws2425/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package de.hka.ws2425.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
