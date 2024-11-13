1aefdb680742e85f80999d94fe2afc94c57db4d1
sebastianknopf <mail@sebastian-knopf.de>
2024-11-13 11:42:59 +0100

    Übung / Vorlesung III

--- app/src/main/java/de/hka/ws2425/ui/map/MapFragment.java
+++ app/src/main/java/de/hka/ws2425/ui/map/MapFragment.java
@@ -2,15 +2,34 @@ package de.hka.ws2425.ui.map;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 import de.hka.ws2425.R;
 
@@ -18,21 +37,121 @@ public class MapFragment extends Fragment {
 
     private MapViewModel mViewModel;
 
+    private MapView mapView;
+
     public static MapFragment newInstance() {
         return new MapFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MapViewModel.class);
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+
+        this.mapView = root.findViewById(R.id.mapView);
+
+        XYTileSource mapServer = new XYTileSource("MapServer",
+                8,
+                20,
+                256,
+                ".png",
+                new String[]{"https://tileserver.svprod01.app/styles/default/"}
+        );
+
+        String authorizationString = this.getMapServerAuthorizationString(
+                "ws2223@hka",
+                "LeevwBfDi#2027"
+        );
+
+        Configuration
+                .getInstance()
+                .getAdditionalHttpRequestProperties()
+                .put("Authorization", authorizationString);
+
+        this.mapView.setTileSource(mapServer);
+
+        GeoPoint startPoint = new GeoPoint(49.0069, 8.4037);
+
+        IMapController mapController = this.mapView.getController();
+        mapController.setZoom(14.0);
+        mapController.setCenter(startPoint);
+
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        String[] permissions = {
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+        };
+
+        Permissions.check(this.getContext(), permissions, null, null, new PermissionHandler() {
+            @Override
+            public void onGranted() {
+                setupLocationListener();
+            }
+
+            @Override
+            public void onDenied(Context context, ArrayList<String> deniedPermissions) {
+                super.onDenied(context, deniedPermissions);
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void setupLocationListener()
+    {
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(android.location.Location location) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                GeoPoint startPoint = new GeoPoint(latitude, longitude);
+
+                IMapController mapController = mapView.getController();
+                mapController.setCenter(startPoint);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        LocationManager locationManager = (LocationManager) this.getContext().getSystemService(
+                Context.LOCATION_SERVICE
+        );
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                2000,
+                10,
+                locationListener
+        );
+    }
+
+    private String getMapServerAuthorizationString(String username, String password)
+    {
+        String authorizationString = String.format("%s:%s", username, password);
+        return "Basic " + Base64.encodeToString(authorizationString.getBytes(StandardCharsets.UTF_8), Base64.DEFAULT);
+    }
 }
