dbe574d673a28d06af3d64354070283fca31bc8a
Silvano <silvano.tripisciano@outlook.com>
2025-01-11 22:02:22 +0100

    UI für Abfahrtsanzeige funktionsfähig

--- app/src/main/java/de/hka/ws2425/utils/DepartureAdapter.java
+++ app/src/main/java/de/hka/ws2425/utils/DepartureAdapter.java
@@ -7,6 +7,9 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import de.hka.ws2425.R;
@@ -15,6 +18,8 @@ import io.reactivex.annotations.NonNull;
 public class DepartureAdapter extends RecyclerView.Adapter<DepartureAdapter.DepartureViewHolder> {
 
     private List<Departure> departures;
+    private final SimpleDateFormat inputFormat = new SimpleDateFormat("HH:mm:ss");
+    private final SimpleDateFormat outputFormat = new SimpleDateFormat("HH:mm");
 
     public DepartureAdapter(List<Departure> departures) {
         this.departures = departures;
@@ -33,13 +38,26 @@ public class DepartureAdapter extends RecyclerView.Adapter<DepartureAdapter.Depa
         Departure departure = departures.get(position);
         holder.routeShortName.setText(departure.getRouteShortName());
         holder.tripHeadsign.setText(departure.getTripHeadsign());
-        holder.arrivalTime.setText(departure.getArrivalTime());
-        holder.departureTime.setText(departure.getDepartureTime());
+
+        // Formatieren der Zeiten
+        holder.arrivalTime.setText(formatTime(departure.getArrivalTime()) + " Uhr");
+        holder.departureTime.setText(formatTime(departure.getDepartureTime()) + " Uhr");
     }
 
     @Override
     public int getItemCount() {
         return departures.size();
+    }
+
+    // Methode zum Formatieren der Uhrzeiten
+    private String formatTime(String time) {
+        try {
+            Date date = inputFormat.parse(time);
+            return outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return time; // Falls ein Fehler auftritt, bleibt die Originalzeit
+        }
     }
 
     static class DepartureViewHolder extends RecyclerView.ViewHolder {
@@ -54,3 +72,4 @@ public class DepartureAdapter extends RecyclerView.Adapter<DepartureAdapter.Depa
         }
     }
 }
+
